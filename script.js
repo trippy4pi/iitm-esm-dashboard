80c1fdd597faf460cccf083074232c8c322f7a83
Abhishek B. Sarbhukan <sarbhukanabhishek@gmail.com>
2026-04-12 02:06:10 +0530

    styled header

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const map = L.map('map').setView([22.9734, 78.6569], 5); // India center
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 8,
+    minZoom: 5,
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
